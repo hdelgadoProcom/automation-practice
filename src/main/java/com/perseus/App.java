a0f90920c1c2068265db3e9d5770dcddb7a6f22c
hdelgadoProcom <hdelgado@procomconsulting.com>
2022-04-19 09:55:04 -0600

    first commit

---
+++ src/main/java/com/perseus/App.java
@@ -0,0 +1,13 @@
+package com.perseus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
